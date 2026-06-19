b662a71602e18e9a3459fb6556ae233c82bf27fd
sudhanshuyadav767 <engineerthink767@gmail.com>
2026-06-19 12:02:40 +0530

    Update Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -47,19 +47,19 @@ export default function App() {
   const activeCount = todos.filter(t => !t.completed).length;
 
   return (
-    <div className="flex flex-col md:flex-row h-screen bg-black text-white antialiased font-sans">
-      <div className="w-full md:w-1/2 bg-black border-r border-red-800 p-6 sm:p-10 flex flex-col h-1/2 md:h-full pt-12 md:pt-10">
+    <div className="flex flex-col md:flex-row min-h-screen bg-black text-white antialiased font-sans">
+      <div className="w-full md:w-1/2 bg-black md:border-r border-red-800 p-6 sm:p-10 flex flex-col md:h-screen pt-12 md:pt-10">
         <div className="max-w-lg mx-auto w-full">
-          <h2 className="text-2xl md:text-4xl font-extrabold text-red-500 mb-8 border-b border-red-800 pb-4">
+          <h2 className="text-xl md:text-4xl font-extrabold text-red-500 mb-8 border-b border-red-800 pb-4">
             Create Your <span className="text-white">To-DO List</span>
           </h2>
           <TodoInput onAddTodo={handleAddTodo} />
         </div>
       </div>
 
-      <div className="w-full md:w-1/2 p-6 sm:p-10 overflow-y-auto bg-gray-900 relative h-1/2 md:h-full border-t md:border-t-0 border-red-800">
+      <div className="w-full md:w-1/2 p-6 sm:p-10 md:overflow-y-auto bg-gray-900 relative md:h-screen border-t md:border-t-0 border-red-800">
         <div className="max-w-3xl mx-auto">
-          <header className="mb-8 border-b border-red-900/50 pb-4">
+          <header className="mt-8 md:mt-0 mb-8 border-b border-red-900/50 pb-4">
             <h1 className="text-4xl font-extrabold text-white tracking-tight mb-2">
             <span className="text-red-500">Your</span> Tasks
             </h1>
